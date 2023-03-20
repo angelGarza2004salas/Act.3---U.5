041b0ec37c53fc26d689cdab6509a9dbae4c4078
angelGarza2004salas <125837002+angelGarza2004salas@users.noreply.github.com>
2023-03-20 12:07:25 -0600

    Add files via upload

---
+++ LaSalida/LaSalida/Salida.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaSalida
+{
+    class Salida
+    {
+        public string[] Figura()
+        {
+            int a = 4;
+            string f, s = "";
+            string[] figura = new string[7];
+            for (int i = 1; i <= 4; i++)
+            {
+                f = s + i;
+                figura[i-1] = f;
+                s = f + " ";
+            }
+            for (int j = 3; j >= 1; j--)
+            {
+                s = figura[j-1];
+                figura[a] = s;
+                a++;
+            }
+            return figura;
+        }
+    }
+}
